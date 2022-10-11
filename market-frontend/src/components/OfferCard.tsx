4e9242241d32718e1a691fd60af8203d54806b9f
Amovane <amovane@163.com>
2022-10-12 00:50:35 +0800

    Tweak card ui

--- market-frontend/src/components/OfferCard.tsx
+++ market-frontend/src/components/OfferCard.tsx
@@ -18,6 +18,17 @@ export function OfferCard({ data: offer, onClick }: CardProps) {
       <div className="card-body bg-base-100">
         <h2 className="card-title">{offer.token.name}</h2>
         <TooltipSection text={offer.token.description} />
+        <div
+          className="justify-end text-pink-400 font-bold"
+          style={{
+            textOverflow: "ellipsis",
+            overflow: "hidden",
+            display: "inline",
+            maxWidth: "10rem",
+          }}
+        >
+          {offer.seller}
+        </div>
         <div className="justify-start text-pink-400 font-bold">
           {offer.price} APT
         </div>
